0227417534a520070877c744f2a51dba288561e3
orber3 <orber3@gmail.com>
2021-09-13 11:21:32 +0300

    changed favicon and title

--- src/Actions/Constants.js
+++ src/Actions/Constants.js
@@ -27,4 +27,4 @@ export const GEO_REQUEST = 'GEO_REQUEST';
 export const GEO_SUCCESS = 'GEO_SUCCESS';
 export const GEO_FAIL = 'GEO_FAIL';
 
-export const API = 'ainxx3TAcnl0c5o0mGsnuyxmSKmSCWJi';
+export const API = 'OqpM8TA54A8TD1FtF03XUUdN5wGA8bSm';
